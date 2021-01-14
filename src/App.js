ddc599ce7fb3a76c887352e123e9fe5429cb1e76
Aditya <ak13984@yahoo.com>
2021-01-14 13:56:11 +0530

    grocerylist first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState,useEffect,useRef} from 'react'
+const placeholder="e.g. eggs";
 
-function App() {
+const App=()=>{
+const [items,setItems]=useState([]);
+const [itemlistEmpty,setListEmpty]=useState(false);
+const [itemEditing, setIsItemEditing]=useState(false);
+const [idtobeEdited, setidtobeEdited]=useState(0);
+const [submit,setSubmit]=useState(false);
+
+
+const myRef=useRef('');
+
+const handleSubmit=()=>{
+  if(itemEditing){
+      const newItems=items.map((item)=>{
+       if(item.key===idtobeEdited){
+          item.name=myRef.current.value;   
+       }
+       return item;
+      })
+  myRef.current.value='';   
+   setIsItemEditing(false);
+      setItems(newItems);
+      setSubmit(true);
+  return;
+    }
+
+  setItems([...items,{name:myRef.current.value,key:items.length+1}]);
+  myRef.current.value='';
+  if(!itemlistEmpty)
+  setListEmpty(true);
+  setSubmit(true);
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div class="card" style={{ width: "25rem", marginTop:'5rem'}}>
+        <h2 style={{display:'flex', justifyContent:'center',height:'5rem', lineHeight:'5rem'}}>Grocery List</h2>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <input type="text" placeholder={placeholder} ref={myRef} style={{width:'90%'}} />
+          <button type="button" onClick={handleSubmit}>
+            {!itemEditing && <p>Submit</p>}
+            {itemEditing && <p>Edit</p>}
+          </button>
+        </div>
+        {itemlistEmpty && (
+          <ul class="list-group">
+            {items.map((item) => {
+              return (
+                <li
+                  className="list-group-item"
+                  style={{ display: "flex", justifyContent: "space-between" }}
+                >
+                  <h3>{item.name}</h3>
+                  <div>
+                    <button
+                      type="button"
+                      class="btn btn-success btn-sm"
+                      style={{ marginRight: "0.5rem" }}
+                      onClick={() => {
+                        myRef.current.value = item.name;
+                        setIsItemEditing(true);
+                        setidtobeEdited(item.key);
+                      }}
+                    >
+                      Edit text
+                    </button>
+                    <button
+                      type="button"
+                      class="btn btn-danger btn-sm"
+                      onClick={() => {
+                        setItems(
+                          items.filter((sitem) => sitem.key !== item.key)
+                        );
+                      }}
+                    >
+                      Remove
+                    </button>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        )}
+        <button
+          type="button" style={{height:'2rem'}}
+          onClick={() => window.location.reload()}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <p style={{color:'red'}}>Clear all items</p>
+        </button>
+      </div>
+    </>
   );
 }
 
